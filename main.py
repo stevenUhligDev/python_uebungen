6200eb742fd8e5a0e94fa49a5624c4f1dea5b3d1
stevenUhligDev <177617514+stevenUhligDev@users.noreply.github.com>
2026-05-31 17:19:01 +0200

    add greeting function

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 name = "Steven"
 system = "Raspberry Pi 5"
-print(f"Hallo {name}, dein Python läuft auf dem {system}.")
+
+
+def create_greeting(name, system):
+    message = f"Hallo {name}, dein Python läuft auf dem {system}."
+    return message
+    
+    
+message = create_greeting(name, system)
+print(message)
