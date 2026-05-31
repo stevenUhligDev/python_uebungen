7ed6dba7191c787d6e253e7bb6af460179cd9f61
stevenUhligDev <177617514+stevenUhligDev@users.noreply.github.com>
2026-05-31 17:19:01 +0200

    Normalize user input

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-name = "Steven"
-system = "Raspberry Pi 5"
-language_de = "de"
-language_en = "en"
 
 
 def create_greeting(name, system, language):
     match language:
-        case "de":
+        case "german":
             message = f"Hallo {name}, dein Python läuft auf dem {system}."
             return message
-        case "en":
+        case "english":
             message = f"Hello {name}, your Python runs on a {system}."
             return message    
         case _:
@@ -17,6 +13,8 @@ def create_greeting(name, system, language):
             return message
     
     
-
-message = create_greeting(name, system, "nl")
+name = input("Enter your name \n").strip()
+system = input("Which system do you use? \n").strip()
+language = input("Which language do you speak? \n").strip().lower()
+message = create_greeting(name, system, language)
 print(message)
