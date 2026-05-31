49f525d1ae0e587e178f7fdfb5b46d0b85133f28
stevenUhligDev <177617514+stevenUhligDev@users.noreply.github.com>
2026-05-31 17:19:01 +0200

    Add German and English greeting messages using match case

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 name = "Steven"
 system = "Raspberry Pi 5"
+language_de = "de"
+language_en = "en"
 
 
-def create_greeting(name, system):
-    message = f"Hallo {name}, dein Python läuft auf dem {system}."
-    return message
+def create_greeting(name, system, language):
+    match language:
+        case "de":
+            message = f"Hallo {name}, dein Python läuft auf dem {system}."
+            return message
+        case "en":
+            message = f"Hello {name}, your Python runs on a {system}."
+            return message    
+        case _:
+            message = "Not matched"
+            return message
     
     
-message = create_greeting(name, system)
+
+message = create_greeting(name, system, "nl")
 print(message)
